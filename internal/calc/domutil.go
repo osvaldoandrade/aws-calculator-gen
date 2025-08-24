4d8eb097a398edee2868c6a0b1e5533c4b80bc5d
Osvaldo Andrade <osvaldo.andrade@gmail.com>
2025-08-24 09:24:19 -0600

    Use search-based selectors and add missing steps

--- internal/calc/domutil.go
+++ internal/calc/domutil.go
@@ -2,26 +2,24 @@ package calc
 
 import "github.com/chromedp/chromedp"
 
-// The functions in this file are placeholders and should be expanded with
-// robust DOM interaction helpers.
+// The functions in this file provide light abstractions over common DOM
+// interactions. They intentionally rely on chromedp's "search" based querying
+// to traverse shadow roots without injecting scripts that depend on browser
+// features such as SharedArrayBuffer.
 
-func Click(sel string, isXPath bool) chromedp.Action {
-	if isXPath {
-		return chromedp.Click(sel, chromedp.BySearch)
-	}
-	return chromedp.Click(sel)
+// Click dispatches a click event using a selector that can be CSS, XPath or
+// plain text.
+func Click(sel string) chromedp.Action {
+	return chromedp.Click(sel, chromedp.BySearch)
 }
 
-func SetValue(sel, value string, isXPath bool) chromedp.Action {
-	if isXPath {
-		return chromedp.SetValue(sel, value, chromedp.BySearch)
-	}
-	return chromedp.SetValue(sel, value)
+// SetValue assigns a value to an element located via CSS, XPath or text
+// selector.
+func SetValue(sel, value string) chromedp.Action {
+	return chromedp.SetValue(sel, value, chromedp.BySearch)
 }
 
-func WaitVisible(sel string, isXPath bool) chromedp.Action {
-	if isXPath {
-		return chromedp.WaitVisible(sel, chromedp.BySearch)
-	}
-	return chromedp.WaitVisible(sel)
+// WaitVisible waits until the element matching the selector is visible.
+func WaitVisible(sel string) chromedp.Action {
+	return chromedp.WaitVisible(sel, chromedp.BySearch)
 }
--- internal/calc/selectors.go
+++ internal/calc/selectors.go
@@ -1,13 +1,15 @@
 package calc
 
 const (
-	SearchInputCSS     = "input[placeholder*='Search' i],input[aria-label*='search' i],input[type='search']"
-	EC2ConfigureXPath  = "//div[.//h3[contains(., 'Amazon EC2')]]//button[contains(., 'Configure') or .//span[contains(., 'Configure')]]"
-	SaveAndAddXPath    = "//button[contains(., 'Save and add service') or .//span[contains(., 'Save and add service')]]"
-	EditNameLinkCSS    = "a[data-cy='edit-estimate-name']"
-	EstimateNameInput  = "input[aria-label='Enter Name']"
-	SaveNameButton     = "button[aria-label='Save']"
-	ShareButtonXPath   = "//button[contains(., 'Share') or .//span[contains(., 'Share')]]"
-	ShareLinkXPath     = "//input[contains(@value, 'https://calculator.aws/#/estimate/')]"
-	NumberInstancesCSS = "input[aria-label*='Number of instances' i]"
+	SearchInputCSS      = "input[placeholder*='Search' i],input[placeholder*='Find' i],input[aria-label*='search' i],input[aria-label*='find' i],input[type='search']"
+	EC2ConfigureXPath   = "//div[.//h3[contains(., 'Amazon EC2')]]//button[contains(., 'Configure') or .//span[contains(., 'Configure')]]"
+	SaveAndAddXPath     = "//button[contains(., 'Save and add service') or .//span[contains(., 'Save and add service')]]"
+	ViewSummaryXPath    = "//span[contains(., 'View summary')]"
+	EditNameLinkCSS     = "a[data-cy='edit-estimate-name']"
+	EstimateNameInput   = "input[aria-label='Enter Name']"
+	SaveNameButton      = "button[aria-label='Save']"
+	ShareButtonXPath    = "//button[contains(., 'Share') or .//span[contains(., 'Share')]]"
+	ShareLinkXPath      = "//input[contains(@value, 'https://calculator.aws/#/estimate/')]"
+	NumberInstancesCSS  = "input[aria-label*='Number of instances' i]"
+	CreateEstimateXPath = "//span[contains(., 'Create estimate')]"
 )
